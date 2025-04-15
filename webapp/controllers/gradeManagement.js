8f3fbe867e8f41ab65a28ca68070dbdd0a9cbaaa
Enda <ehamiltonfitzpatrick01@qub.ac.uk>
2025-04-15 11:18:35 +0100

    Added toggle in grades ejs to render grades by either student or module

--- webapp/controllers/gradeManagement.js
+++ webapp/controllers/gradeManagement.js
@@ -21,10 +21,34 @@ router.get('/', async (req, res) => {
         });
 
     } catch (err) {
-        console.error("Error loading grades:", err.message);
-        res.status(500).send("Error loading grades");
+        console.error("Error loading grades by student:", err.message);
+        res.status(500).send("Error loading grades by student");
     }
 
 });
+
+//GET all grades grouped by module
+router.get('/by-module', async (req, res) => {
+
+    try {
+        const gradeRes = await axios.get('http://localhost:4000/grades/by-module', config);
+
+        res.render('grademanagement', {
+            user: {
+                id: req.session.userID,
+                email: req.session.email
+            },
+            groupBy: 'module',
+            groupedByModule: gradeRes.data
+        });
+
+    } catch (err) {
+        console.error("Error loading grades by module:", err.message);
+        res.status(500).send("Error loading grades by module");
+    }
+
+});
+
+
 
 module.exports = router;
